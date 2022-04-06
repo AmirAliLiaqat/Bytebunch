6bd1bbcaffd3251539bc6b71aa958710b7e0a377
Amir Ali <amirliaqat2020@gmail.com>
2022-04-06 11:40:06 +0500

    Creating Page Files Dymamic

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Page File.
+ *
+ * @package ByteBunch
+*/
+get_header();
+?>
+<?php
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+?>
+    <!-------------------- Section Start -------------------->
+    <section>
+        <div class="container my-4">
+            <div class="row mt-4">
+                <div class="col-md-8 login-col">
+                    <div class="my-2 p-3" style="background: #fff;"><h3><?php the_title(); ?></h3></div>
+                    <div class="my-4 p-3" style="background: #fff;">
+                        <?php 
+                            if(the_content() == "") {
+                                echo "<p>There is nothing on this page to show you.</p>";
+                            } else {
+                                the_content(); 
+                            }
+                        ?>
+                    </div>
+                </div><!--col-md-8-->
+                <div class="col-md-4 login-col">
+                    <?php get_sidebar(); ?>
+                </div><!--col-md-4-->
+            </div><!--row-->
+        </div><!--container-->
+    </section>
+    <!-------------------- Section End -------------------->
+<?php 
+        }
+    }
+?>
+<?php get_footer(); ?>
