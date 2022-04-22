3a8f5638ab6ebfa4857b8098a51b0699403e6cc6
Amir Ali <amirliaqat2020@gmail.com>
2022-04-22 15:44:01 +0500

    Creating Forgot Password Template

--- template-forgot-password.php
+++ template-forgot-password.php
@@ -20,13 +20,28 @@ get_header();
                         <h3><?php the_title(); ?></h3>
                     </div><!--my-2-->
                     <div class="my-4 p-3" style="background: #fff;">
-                        <?php 
-                            if(the_content() == "") {
-                                echo "<p>There is nothing on this page to show you.</p>";
-                            } else {
-                                the_content(); 
-                            }
-                        ?>
+                        <div class="web_boxp">
+                            <form action="" class="jquery_validated_form" method="post">
+                                    <p>
+                                        <label for="password">
+                                            <strong>E-mail: </strong>
+                                            <span class="forum_star">*</span>
+                                        </label><br>
+                                        <small>This must be the e-mail address associated with your account. If you have not changed this via your user control panel then it is the e-mail address you registered your account with.</small>
+                                        <input type="text" class="w-50" name="user_email" id="email" class="required" required="required">
+                                    </p>
+                                    <p>
+                                    </p>
+                                    <p>
+                                        <!-- <label for="captcha"><strong>Captcha: <span class="forum_star">*</span></strong></label> -->
+                                        <img src="https://test.bytebunch.com/wp-content/themes/bbblog/lib/captcha/contact.php" alt="captcha code" style="margin-bottom:10px;border:1px solid #ccc; display:block;">
+                                        <input type="text" class="w-50" name="captcha" id="captcha" autocomplete="off" required="required">
+                                        <!-- <small>Type the digits shown in above image.</small> -->
+                                    </p>
+                                <p><input type="submit" value="Submit" style="width:30% !important;"></p>
+                            </form>
+                            <div class="clearboth"></div>
+                        </div><!--web_boxp-->
                     </div><!--my-2-->
                 </div><!--col-md-8-->
                 <div class="col-md-4 login-col">
