1d71ec2919ab9c6319b073857e61ca7c109081f3
Amir Ali <amirliaqat2020@gmail.com>
2022-04-23 10:05:51 +0500

    Adding Login Page Widgets Areas.

--- functions.php
+++ functions.php
@@ -57,6 +57,24 @@ function bytebunch_widgets_init() {
     ) );
  
     register_sidebar( array(
+        'name'                =>  __( 'Login Widget 1', 'bytebunch' ),
+        'id'                     =>  'login-1',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h2 class="widget-title">',
+        'after_title'      =>  '</h2>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Login Widget 2', 'bytebunch' ),
+        'id'                     =>  'login-2',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h2 class="widget-title">',
+        'after_title'      =>  '</h2>',
+    ) );
+ 
+    register_sidebar( array(
         'name'                =>  __( 'Footer Widget 1', 'bytebunch' ),
         'id'                     =>  'footer-1',
         'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
