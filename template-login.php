a06dc50460a7251787f18d0a2f0e3022a669a008
Amir Ali <amirliaqat2020@gmail.com>
2022-04-23 10:06:09 +0500

    Making Login Template Dynamic.

--- template-login.php
+++ template-login.php
@@ -21,42 +21,10 @@ get_header();
             </div><!--row-->
             <div class="row mt-4" style="background-color: #fff;">
                 <div class="col-md-6 login-col">
-                    <form action="" method="post">
-                        <p>
-                            <label for="username">Username:</label> <br>
-                            <input type="text" id="username" required>
-                        </p>
-                        <p>
-                            <label for="password">Passowrd:</label> <br>
-                            <input type="password" id="password" required>
-                        </p>
-                        <p>
-                            <input type="checkbox" id="remember_me"  style="width:5% !important;" checked>
-                            <label for="remember_me">Log me on automatically each visit</label>
-                        </p>
-                        <p>
-                            <a href="">Forgot your password?</a>
-                        </p>
-                        <p>
-                            <input type="submit" value="Log In" class="small-btn-Ube" style="width:20% !important;">
-                        </p>
-                    </form>
+                    <?php dynamic_sidebar('login-1'); ?>
                 </div><!--col-md-6-->
                 <div class="col-md-6 login-col">
-                    <h2>New User</h2>
-                    <p>
-                        In order to login you must be registered. Registering takes only a few moments 
-                        but gives you increased capabilities. The board administrator may also grant 
-                        additional permissions to registered users. Before you register please ensure 
-                        you are familiar with our terms of use and related policies. Please ensure you 
-                        read any forum rules as you navigate around the board.
-                    </p>
-                    <p>
-                        <a href="">Terms of use</a> | <a href="">Privacy policy</a>
-                    </p>
-                    <p>
-                        <a href="" class="orange-btn">Create My Account</a>
-                    </p>
+                    <?php dynamic_sidebar('login-2'); ?>
                 </div><!--col-md-6-->
             </div><!--row-->
         </div><!--container-->
