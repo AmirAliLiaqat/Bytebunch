6dfe6530114f66450150f5f6ee480e39e3904032
Amir Ali <amirliaqat2020@gmail.com>
2022-05-02 13:01:34 +0500

    Adding Code for customizer js

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+(function( $ ) {
+	"use strict";
+
+	// Header Background Color - Color Control
+	wp.customize( 'header_background_color_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-header' ).css( 'backgroundColor', to );
+		} );
+	});
+
+    // Header Background Image - Image Control
+	wp.customize( 'header_background_image_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-header' ).css( 'background-image', 'url( ' + to + ')' );
+		} );
+	});
+
+	// Body Background Color - Color Control
+	wp.customize( 'body_background_color_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-body' ).css( 'backgroundColor', to );
+		} );
+	});
+
+    // Body Background Image - Image Control
+	wp.customize( 'body_background_image_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-header' ).css( 'background-image', 'url( ' + to + ')' );
+		} );
+	});
+
+	// Footer Background Color - Color Control
+	wp.customize( 'footer_background_color_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-footer' ).css( 'backgroundColor', to );
+		} );
+	});
+
+    // Footer Background Image - Image Control
+	wp.customize( 'footer_background_image_setting', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-header' ).css( 'background-image', 'url( ' + to + ')' );
+		} );
+	});
+
+})( jQuery );
